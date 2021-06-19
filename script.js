3f5bd80287fcb0dd9b5b5584f9393306ed7f823d
wangsh <wugcelama19@gmail.com>
2021-06-19 14:32:55 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//Object create.
+let myObj = {};
+
+//variable decleare.
+let a = "apple";
+let b = "red";
+let c = "ball";
+let d = "round";
+let e = "cat";
+let f = "meow";
+let g = "dog";
+let h = "bone";
+
+//object assigned.
+myObj[a] =b;
+myObj[c] =d;
+myObj[e] =f;
+myObj[g] =h;
+
+//print object.   
+console.log(myObj);
+
+
+
+
+
+
